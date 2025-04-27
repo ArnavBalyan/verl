94a65ec2c0a1da94447b3d90d761bf8f2473d960
Arnav Balyan <arnavb@uber.com>
2025-04-27 20:44:27 +0530

    update

--- verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
+++ verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
@@ -250,11 +250,13 @@ class vLLMRollout(BaseRollout):
         t0 = time.time()
         with self.update_sampling_params(**kwargs):
             t1 = time.time()
+            augmented_vllm_inputs = vllm_inputs + vllm_inputs
             outputs = self.inference_engine.generate(
-                prompts=vllm_inputs,
+                prompts=augmented_vllm_inputs,
                 sampling_params=self.sampling_params,
                 use_tqdm=False)
             print(f"[Timing][VLLM] Inference engine generation done in {time.time() - t1:.4f} seconds")
+            outputs = outputs[:len(vllm_inputs)]
 
             t1 = time.time()
             response = []
